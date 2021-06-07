646f3d8356fb36776e2adb9bd73840e4abd7189e
FantasyWinds <1311833284@qq.com>
2021-06-07 22:56:10 +0800

    add global symboltable

--- src/symtab.cpp
+++ src/symtab.cpp
@@ -1,189 +1,202 @@
 #include "symtab.h"
 
-Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int arrayLength, int scopeIndex){
-	this->id = id;
-	this->symbolType = valType;
-	this->symbolClass = symbolType;
-	this->node = node;
-	this->elementType = elementType;
-	this->arrayLength = arrayLength;
-	this->beginIndex = 0;
-	this->endIndex = beginIndex + arrayLength;
-	this->scopeIndex = scopeIndex;
-	this->null = 0;
-}
+namespace TOY_COMPILER {
 
-Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int beginIndex, int endIndex, int scopeIndex) {
-	this->id = id;
-	this->symbolType = valType;
-	this->symbolClass = symbolType;
-	this->node = node;
-	this->elementType = elementType;
-	this->arrayLength = endIndex-beginIndex+1;
-	this->beginIndex = beginIndex;
-	this->endIndex = endIndex;
-	this->scopeIndex = scopeIndex;
-	this->null = 0;
-}
+	SymbolTable* GlobalSymbol::NewSymTable(std::string tablename) {
+		SymbolTable *NewSymTable = new SymbolTable(tablename);
+		this->SymTable.insert(std::map<std::string, class SymbolTable*>::value_type(tablename, NewSymTable));
+	}
 
+	SymbolTable* GlobalSymbol::NewSymTable(std::string tablename, SymbolTable *parent) {
+		SymbolTable *NewSymTable = new SymbolTable(tablename);
+		this->SymTable.insert(std::map<std::string, class SymbolTable*>::value_type(tablename, NewSymTable));
+	}
 
-Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex) {
-	this->id = id;
-	this->symbolType = valType;
-	this->symbolClass = symbolType;
-	this->node = node;
-	this->scopeIndex = scopeIndex;
-	this->null = 0;
-}
+	Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int arrayLength, int scopeIndex) {
+		this->id = id;
+		this->symbolType = valType;
+		this->symbolClass = symbolType;
+		this->node = node;
+		this->elementType = elementType;
+		this->arrayLength = arrayLength;
+		this->beginIndex = 0;
+		this->endIndex = beginIndex + arrayLength;
+		this->scopeIndex = scopeIndex;
+		this->null = 0;
+	}
 
-Symbol::Symbol(std::string id, TOY_COMPILER::const_valueType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex) {
-	this->id = id;
-	this->symbolType = valType.d_type;
-	this->constSys = valType.sys_type;
-	this->symbolClass = symbolType;
-	this->node = node;
-	this->scopeIndex = scopeIndex;
-	this->null = 0;
-}
-
-Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int beginIndex, int endIndex, int scopeIndex) {
-	this->id = id;
-	this->symbolType = valType;
-	this->symbolClass = symbolType;
-	this->node = node;
-	this->beginIndex = beginIndex;
-	this->endIndex = endIndex;
-	this->scopeIndex = scopeIndex;
-	this->null = 0;
-}
-
-Symbol::Symbol(std::string id, TOY_COMPILER::abstractAST *node, int scopeIndex) {
-	this->id = id;
-	this->node = node;
-	this->scopeIndex = scopeIndex;
-	this->null = 0;
-}
-
-Symbol::Symbol(std::string name) {
-	this->name = name;
-	this->symbolClass = TOY_COMPILER::ELEMENT_S;
-	this->null = 0;
-}
+	Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int beginIndex, int endIndex, int scopeIndex) {
+		this->id = id;
+		this->symbolType = valType;
+		this->symbolClass = symbolType;
+		this->node = node;
+		this->elementType = elementType;
+		this->arrayLength = endIndex - beginIndex + 1;
+		this->beginIndex = beginIndex;
+		this->endIndex = endIndex;
+		this->scopeIndex = scopeIndex;
+		this->null = 0;
+	}
 
 
-Symbol::Symbol() {
-	this->null = 1;
-}
+	Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex) {
+		this->id = id;
+		this->symbolType = valType;
+		this->symbolClass = symbolType;
+		this->node = node;
+		this->scopeIndex = scopeIndex;
+		this->null = 0;
+	}
+
+	Symbol::Symbol(std::string id, TOY_COMPILER::const_valueType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex) {
+		this->id = id;
+		this->symbolType = valType.d_type;
+		this->constSys = valType.sys_type;
+		this->symbolClass = symbolType;
+		this->node = node;
+		this->scopeIndex = scopeIndex;
+		this->null = 0;
+	}
+
+	Symbol::Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int beginIndex, int endIndex, int scopeIndex) {
+		this->id = id;
+		this->symbolType = valType;
+		this->symbolClass = symbolType;
+		this->node = node;
+		this->beginIndex = beginIndex;
+		this->endIndex = endIndex;
+		this->scopeIndex = scopeIndex;
+		this->null = 0;
+	}
+
+	Symbol::Symbol(std::string id, TOY_COMPILER::abstractAST *node, int scopeIndex) {
+		this->id = id;
+		this->node = node;
+		this->scopeIndex = scopeIndex;
+		this->null = 0;
+	}
+
+	Symbol::Symbol(std::string name) {
+		this->name = name;
+		this->symbolClass = TOY_COMPILER::ELEMENT_S;
+		this->null = 0;
+	}
 
 
-FunctionSymbol::FunctionSymbol(std::string functionName) {
-	this->functionName = functionName;
-}
+	Symbol::Symbol() {
+		this->null = 1;
+	}
 
 
-FunctionSymbol::FunctionSymbol() {
-	this->null = 1;
-}
+	FunctionSymbol::FunctionSymbol(std::string functionName) {
+		this->functionName = functionName;
+	}
 
-int SymbolList::insertSym(Symbol s) {
-	this->list.push_front(s);
-	return 0;
-}
 
-int SymbolList::deleteSym(std::string delname) {
-	std::list<class Symbol>::iterator s = this->list.begin();
-	while (s != this->list.end()) {
-		if (s->id == delname) {
-			this->list.erase(s);
-			return 0;
+	FunctionSymbol::FunctionSymbol() {
+		this->null = 1;
+	}
+
+	int SymbolList::insertSym(Symbol s) {
+		this->list.push_front(s);
+		return 0;
+	}
+
+	int SymbolList::deleteSym(std::string delname) {
+		std::list<class Symbol>::iterator s = this->list.begin();
+		while (s != this->list.end()) {
+			if (s->id == delname) {
+				this->list.erase(s);
+				return 0;
+			}
+			s++;
 		}
-		s++;
+		return 1;
 	}
-	return 1;
-}
-int SymbolList::insertFunction(FunctionSymbol function) {
-	std::list<class FunctionSymbol>::iterator fun = this->functionList.begin();
-	std::string id = function.functionName;
-	while (fun != this->functionList.end()) {
-		if ((*fun).functionName == id)return -1;
-		fun++;
+	int SymbolList::insertFunction(FunctionSymbol function) {
+		std::list<class FunctionSymbol>::iterator fun = this->functionList.begin();
+		std::string id = function.functionName;
+		while (fun != this->functionList.end()) {
+			if ((*fun).functionName == id)return -1;
+			fun++;
+		}
+		this->functionList.push_front(function);
 	}
-	this->functionList.push_front(function);
-}
 
-FunctionSymbol SymbolList::findFunction(std::string id) {
-	std::list<class FunctionSymbol>::iterator fun = this->functionList.begin();
-	while (fun != this->functionList.end()) {
-		if ((*fun).functionName == id)return *fun;
-		fun++;
+	FunctionSymbol SymbolList::findFunction(std::string id) {
+		std::list<class FunctionSymbol>::iterator fun = this->functionList.begin();
+		while (fun != this->functionList.end()) {
+			if ((*fun).functionName == id)return *fun;
+			fun++;
+		}
+		return FunctionSymbol();
 	}
-	return FunctionSymbol();
-}
 
-Symbol SymbolList::findSym(std::string id) {
-	std::list<class Symbol>::iterator sym = this->list.begin();
-	while (sym != this->list.end()) {
-		if (sym->id == id)return *sym;
+	Symbol SymbolList::findSym(std::string id) {
+		std::list<class Symbol>::iterator sym = this->list.begin();
+		while (sym != this->list.end()) {
+			if (sym->id == id)return *sym;
+		}
+		return Symbol();
 	}
-	return Symbol();
-}
-	
-int SymbolTable::insertSym(Symbol s){	
-	int hashresult = SymbolTable::string_hash(s.id);
-	SymbolList *list = this->table[hashresult];
-	if (list == NULL)list = new SymbolList();
-	list->insertSym(s);
-	return 0;
-}
 
-int SymbolTable::insertFunction(FunctionSymbol fun) {
-	int hashresult = SymbolTable::string_hash(fun.functionName);
-	SymbolList *list = this->table[hashresult];
-	if (list == NULL)list = new SymbolList();
-	list->insertFunction(fun);
-	return 0;
-}
-
-int SymbolTable::deleteSym(std::string delname) {
-	int hashresult = SymbolTable::string_hash(delname);
-	SymbolList *list = this->table[hashresult];
-	if (list == NULL)list = new SymbolList();
-	list->deleteSym(delname);
-	return 0;
-}
-
-
-SymbolTable::~SymbolTable() {
-	SymbolTable *partab = this->parentTable;
-	for (int i = 0; i < 100; i++)if (this->table[i] != NULL)delete this->table[i];
-}
-
-SymbolTable::SymbolTable(std::string tablename) {
-	for (int i = 0; i < 100; i++) {
-		this->table[i] = NULL;
+	int SymbolTable::insertSym(Symbol s) {
+		int hashresult = SymbolTable::string_hash(s.id);
+		SymbolList *list = this->table[hashresult];
+		if (list == NULL)list = new SymbolList();
+		list->insertSym(s);
+		return 0;
 	}
-	this->scope = 0;
-	this->parentTable = NULL;
-	this->tablename = tablename;
-}
 
-SymbolTable::SymbolTable(std::string tablename, SymbolTable *parent) {
-	for (int i = 0; i < 100; i++) {
-		this->table[i] = NULL;
+	int SymbolTable::insertFunction(FunctionSymbol fun) {
+		int hashresult = SymbolTable::string_hash(fun.functionName);
+		SymbolList *list = this->table[hashresult];
+		if (list == NULL)list = new SymbolList();
+		list->insertFunction(fun);
+		return 0;
 	}
-	this->scope = parent->scope + 1;
-	this->parentTable = parent;
-	this->scope = parent->scope+1;
-}
 
-Symbol SymbolTable::findSym(std::string id) {
-	SymbolTable *Symtable = this;
-	Symbol SymbolResult=Symbol();
-	int index = this->string_hash(id);
-	while (Symtable != NULL) {
-		SymbolResult=Symtable->table[index]->findSym(id);
-		if (!SymbolResult.null)break;
-		Symtable = Symtable->parentTable;
+	int SymbolTable::deleteSym(std::string delname) {
+		int hashresult = SymbolTable::string_hash(delname);
+		SymbolList *list = this->table[hashresult];
+		if (list == NULL)list = new SymbolList();
+		list->deleteSym(delname);
+		return 0;
 	}
-	return SymbolResult;
+
+
+	SymbolTable::~SymbolTable() {
+		SymbolTable *partab = this->parentTable;
+		for (int i = 0; i < 100; i++)if (this->table[i] != NULL)delete this->table[i];
+	}
+
+	SymbolTable::SymbolTable(std::string tablename) {
+		for (int i = 0; i < 100; i++) {
+			this->table[i] = NULL;
+		}
+		this->scope = 0;
+		this->parentTable = NULL;
+		this->tablename = tablename;
+	}
+
+	SymbolTable::SymbolTable(std::string tablename, SymbolTable *parent) {
+		for (int i = 0; i < 100; i++) {
+			this->table[i] = NULL;
+		}
+		this->scope = parent->scope + 1;
+		this->parentTable = parent;
+		this->scope = parent->scope + 1;
+	}
+
+	Symbol SymbolTable::findSym(std::string id) {
+		SymbolTable *Symtable = this;
+		Symbol SymbolResult = Symbol();
+		int index = this->string_hash(id);
+		while (Symtable != NULL) {
+			SymbolResult = Symtable->table[index]->findSym(id);
+			if (!SymbolResult.null)break;
+			Symtable = Symtable->parentTable;
+		}
+		return SymbolResult;
+	}
 }
--- src/symtab.h
+++ src/symtab.h
@@ -3,108 +3,118 @@
 #include <string>
 #include "types.cuh"
 #include "ast.h"
+#include<map>
 #include <list>
 #define SPL_LIST std::list<class Symbol>
 #define SPL_TABLE std::map<std::string, class SymbolTable*>
 
-class Symbol {	//for symbol except functions
-public :
-	std::string id;	//id of the symbol
-	std::string name;	//name for element
-	TOY_COMPILER::valType symbolType;	//what is the type of the val:INTEGER,REAL,BOOLEAN,CHAR
-	TOY_COMPILER::symbolType symbolClass;	//what is the symbol:CONST,VAR,TYPE,RANGE
-	std::vector<Symbol> memberList;
+namespace TOY_COMPILER {
+	class GlobalSymbol {
+	public:
+		std::map<std::string, class SymbolTable*> SymTable;	//string is the id of the function, use id to find table
+		SymbolTable*  NewSymTable(std::string tablename);	//initiate a table, the first one, without parent
+		SymbolTable*  NewSymTable(std::string tablename, SymbolTable *parent);	//initiate a table, one with a parent, when find a symbol will find from it and its parent
+	};
 
-	TOY_COMPILER::sysCON constSys;	//only for const
 
-	int assigned; //if 1 means has been assigned, used for const
-	int scopeIndex;
-	TOY_COMPILER::abstractAST * node;	//related nodes
+	class Symbol {	//for symbol except functions
+	public:
+		std::string id;	//id of the symbol
+		std::string name;	//name for element
+		TOY_COMPILER::valType symbolType;	//what is the type of the val:INTEGER,REAL,BOOLEAN,CHAR
+		TOY_COMPILER::symbolType symbolClass;	//what is the symbol:CONST,VAR,TYPE,RANGE
+		std::vector<Symbol> memberList;
 
-	TOY_COMPILER::valType elementType;	//if is array, the element type
-	int arrayLevel;		//don't know if it will be used(?)
-	int arrayLength;	//how many elements
-	int beginIndex;		//for range
-	int endIndex;		//for range
+		TOY_COMPILER::sysCON constSys;	//only for const
 
-	std::string error;
+		int assigned; //if 1 means has been assigned, used for const
+		int scopeIndex;
+		TOY_COMPILER::abstractAST * node;	//related nodes
 
-	int null;
-	Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int beginIndex, int endIndex, int scopeIndex);	//add a array with range
-	Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int arrayLength, int scopeIndex);	//add a array with length
-	Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int beginIndex, int endIndex, int scopeIndex); //add a range
-	Symbol(std::string id, TOY_COMPILER::const_valueType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex); //add a const
-	Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex);	//add a integer/real/boolean/char/complex
-	Symbol(std::string id, TOY_COMPILER::abstractAST *node, int scopeIndex);
-	Symbol(std::string name);	//create a symbol only with name, for the element of name declaration
-	Symbol();
-	int SetAssigned() {
-		this->assigned = 1;	//if assigned a const
-	}
-	bool IsAssigned() {
-		return assigned;	//To know if a const has been assigned
-	}
-	int addMem(Symbol s) {	//used to add member
-		memberList.push_back(s);
-	}
-	int changeType(TOY_COMPILER::valType type) {	//used to change the type of the symbol
-		symbolType = type;
-	}
-};
+		TOY_COMPILER::valType elementType;	//if is array, the element type
+		int arrayLevel;		//don't know if it will be used(?)
+		int arrayLength;	//how many elements
+		int beginIndex;		//for range
+		int endIndex;		//for range
 
-class SymbolList {	//a list of symbol
-public:
-    std::list<class Symbol> list;	//list for symbol
-    std::list<class FunctionSymbol> functionList;	//list for function symbols
-    int insertSym(Symbol s);	//add a symbol
-    int insertFunction(FunctionSymbol function);	//add a function
-    int deleteSym(std::string delname);	//maybe useless
-    Symbol findSym(std::string id);	//find a sym in the list
-    FunctionSymbol findFunction(std::string id);	//find a function in the list
+		std::string error;
 
-};
+		int null;
+		Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int beginIndex, int endIndex, int scopeIndex);	//add a array with range
+		Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, TOY_COMPILER::valType elementType, int arrayLength, int scopeIndex);	//add a array with length
+		Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int beginIndex, int endIndex, int scopeIndex); //add a range
+		Symbol(std::string id, TOY_COMPILER::const_valueType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex); //add a const
+		Symbol(std::string id, TOY_COMPILER::valType valType, TOY_COMPILER::symbolType symbolType, TOY_COMPILER::abstractAST *node, int scopeIndex);	//add a integer/real/boolean/char/complex
+		Symbol(std::string id, TOY_COMPILER::abstractAST *node, int scopeIndex);
+		Symbol(std::string name);	//create a symbol only with name, for the element of name declaration
+		Symbol();
+		int SetAssigned() {
+			this->assigned = 1;	//if assigned a const
+		}
+		bool IsAssigned() {
+			return assigned;	//To know if a const has been assigned
+		}
+		int addMem(Symbol s) {	//used to add member
+			memberList.push_back(s);
+		}
+		int changeType(TOY_COMPILER::valType type) {	//used to change the type of the symbol
+			symbolType = type;
+		}
+	};
 
-class SymbolTable {
-public:
-    SymbolList *table[100];	//a hash table, each is a list
-    SymbolTable *parentTable;	//if it is a function/scope table, it will point to the scope defines it
-    int scope;
-    std::string tablename; //which function the table belong
-    int insertSym(Symbol s);	//insert a symbol
-    int insertFunction(FunctionSymbol fun);	//insert a function symbol
-    int deleteSym(std::string delname);	//maybe useless
-    ~SymbolTable();
-    SymbolTable(std::string tablename);	//create a new table with id
-    SymbolTable(std::string tablename, SymbolTable *parent);	//create a new table with id and a parent table
-    Symbol findSym(std::string id);	//find a symbol in this table and its parents table.
-    std::string error; //Useless. Maybe changed in future.
-private:
-    int string_hash(std::string str) {	//for string hash
-        unsigned long hashresult= 0;
-        for (size_t i = 0; i < str.size(); i++)
-            hashresult = 5 * hashresult + str[i];
-        return size_t(hashresult%100);
-    }
-};
+	class SymbolList {	//a list of symbol
+	public:
+		std::list<class Symbol> list;	//list for symbol
+		std::list<class FunctionSymbol> functionList;	//list for function symbols
+		int insertSym(Symbol s);	//add a symbol
+		int insertFunction(FunctionSymbol function);	//add a function
+		int deleteSym(std::string delname);	//maybe useless
+		Symbol findSym(std::string id);	//find a sym in the list
+		FunctionSymbol findFunction(std::string id);	//find a function in the list
 
-class FunctionSymbol {	//only for function symbol
-public:
-	int null;
-	std::string functionName;
-	std::vector<Symbol> args;
-	TOY_COMPILER::valType returnType;	//if it is function, the type of return val
-	SymbolTable *functionTable;	//every function give a divided table
+	};
 
-	TOY_COMPILER::abstractAST * *node;	//related AST nodes
+	class SymbolTable {
+	public:
+		SymbolList *table[100];	//a hash table, each is a list
+		SymbolTable *parentTable;	//if it is a function/scope table, it will point to the scope defines it
+		int scope;
+		std::string tablename; //which function the table belong
+		int insertSym(Symbol s);	//insert a symbol
+		int insertFunction(FunctionSymbol fun);	//insert a function symbol
+		int deleteSym(std::string delname);	//maybe useless
+		~SymbolTable();
+		SymbolTable(std::string tablename);	//create a new table with id
+		SymbolTable(std::string tablename, SymbolTable *parent);	//create a new table with id and a parent table
+		Symbol findSym(std::string id);	//find a symbol in this table and its parents table.
+		std::string error; //Useless. Maybe changed in future.
+	private:
+		int string_hash(std::string str) {	//for string hash
+			unsigned long hashresult = 0;
+			for (size_t i = 0; i < str.size(); i++)
+				hashresult = 5 * hashresult + str[i];
+			return size_t(hashresult % 100);
+		}
+	};
 
-	FunctionSymbol();
-	FunctionSymbol(std::string functionName);	//insert a function only with id, add args and result value with functions below
-	int AddArgs(Symbol s) {	//add a argment
-		this->args.push_back(s);
-	}
-	int SetRet(TOY_COMPILER::valType returnType) {	//set the return type.
-		this->returnType = returnType;
-	}
-};
+	class FunctionSymbol {	//only for function symbol
+	public:
+		int null;
+		std::string functionName;
+		std::vector<Symbol> args;
+		TOY_COMPILER::valType returnType;	//if it is function, the type of return val
+		SymbolTable *functionTable;	//every function give a divided table
 
+		TOY_COMPILER::abstractAST * *node;	//related AST nodes
+
+		FunctionSymbol();
+		FunctionSymbol(std::string functionName);	//insert a function only with id, add args and result value with functions below
+		int AddArgs(Symbol s) {	//add a argment
+			this->args.push_back(s);
+		}
+		int SetRet(TOY_COMPILER::valType returnType) {	//set the return type.
+			this->returnType = returnType;
+		}
+	};
+}
 /*in the semantic class, I have a map for <string, SymbolTable*> to save the tables for the functions*/
